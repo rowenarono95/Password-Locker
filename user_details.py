960e9ecc70b2f04a8013e52fede4dceec27825a1
rowenarono95 <rowenarono@gmail.com>
2020-10-11 21:38:15 +0300

    valid the pass

--- user_details.py
+++ user_details.py
@@ -1,21 +1,31 @@
+user = []
+
 class User:
     def __init__ (self, first_name, second_name, password):
         self.first_name = first_name
         self.second_name = second_name
         self.password = password
 
+    def login():
+        firstname = input("Firstname: ")
+        secondname = input("Secondname: ")
+        password1 = input("Password: ")
+        Credentials.cred()
+        for obj in user:
+            print(obj)
+
+        if obj.firstname == first_name and obj.secondname == second_name and obj.password1 == password:
+            print("twendelee")
+        else:
+            print("Matata")
+
+
     def signup():
         user1 = User(input("Firstname: "), input("Secondname: "), input ("Password: "))
+        user.append("user1")
+        User.login()
 
-
-        
-
-
-
-
-
-
-
+    
 
 
 
@@ -24,7 +34,7 @@ class User:
 
 
 print("Hello, welcome! ")
-print("Choose one of the following: lg for login, su for signup and exit for ex")
+print("Choose one of the following: lg for login, su for signup and ex for exit")
 option = input()
 
 if option == "lg":
@@ -37,9 +47,10 @@ else:
     print("invalid")
 
 
-
-
-
 class Credentials:
-    pass
+    def __init__ (self, first_name, second_name, password):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.password = password
+    print("")
 
