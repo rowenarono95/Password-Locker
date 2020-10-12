1e258711e44c2a80c50d1332a70bc6e8b5009343
rowenarono95 <rowenarono@gmail.com>
2020-10-12 12:35:40 +0300

    generated password

--- user_details.py
+++ user_details.py
@@ -5,21 +5,18 @@ user = []
 details = []
 
 class Credentials:
-    def __init__ (self, site_name, username, password):
+    def __init__ (self, site_name, user_name, password):
         self.site_name = site_name
-        self.username = user_name
+        self.user_name = user_name
         self.password = password
 
-    def password(length):
-        letters = string.ascii_lowercase
-        result_str = ''.join(random.choice(letters) for i in range(10))
 
     def cred():
         print("Choose one of the following")
         print("n\ 1. Put in existing accounts n\ 2. Create new crediantials account n\ 3. View exisiting crediantaials")
         my_option = input()
         if my_option == "1":
-            person1 = input("Account Name: "), input("Username: "), input("Password: ")
+            person1 = Credentials(site_name = input("Account Name: "), user_name = input("Username: "), password = input("Password: "))
             print(person1)
             details.append(person1)
         elif my_option == "2":
@@ -27,9 +24,10 @@ class Credentials:
             print("n\1. Yes n\2. No")
             option = input()
             if option == "1":
-                person2 = (input("Account Name: "), input("Username: "), result_str)
+                pass1 = User.password(10)
+                person2 = Credentials(site_name = input("Account Name: "), user_name = input("Username: "), password = pass1)
                 details.append("person2")
-                print(person2)
+                print(person2.site_name, person2.user_name, person2.password)
             else:
                 print("it's never that serious")
         elif "3":
@@ -73,7 +71,10 @@ class User:
         user.append("user1")
         User.login()
 
-   
+    def password(length):
+        letters = string.ascii_lowercase
+        result = ''.join(random.choice(letters) for i in range(10))
+        return result
 
     
 
