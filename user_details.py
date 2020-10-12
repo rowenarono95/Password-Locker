a6b54df71145891b73f65a9fb96ff91cc99b6439
rowenarono95 <rowenarono@gmail.com>
2020-10-12 16:40:46 +0300

    added a delete method

--- user_details.py
+++ user_details.py
@@ -13,32 +13,36 @@ class Credentials:
 
     def cred():
         print("Choose one of the following")
-        print("n\ 1. Put in existing accounts n\ 2. Create new crediantials account n\ 3. View exisiting crediantaials")
+        print("\n 1. Put in existing accounts \n 2. Create new crediantials account \n 3. View exisiting crediantaials")
         my_option = input()
         if my_option == "1":
             person1 = Credentials(site_name = input("Account Name: "), user_name = input("Username: "), password = input("Password: "))
             details.append(person1)
             User.view()
+            User.delete_cred()
 
         elif my_option == "2":
             print("Do you want an account generated password")
-            print("n\ 1. Yes n\ 2. No")
+            print("\n 1. Yes \n 2. No")
             option = input()
             if option == "1":
                 pass1 = User.password(10)
                 person2 = Credentials(site_name = input("Account Name: "), user_name = input("Username: "), password = pass1)
                 details.append(person2)
                 User.view()
+                User.delete_cred()
             elif option == "2":
                 person2 = Credentials(site_name = input("Account Name: "), user_name = input("Username: "), password = input("Password: "))
                 details.append(person2)
                 User.view()
+                User.delete_cred()
             else:         
                 print("Invalid input")
                 Credentials.cred()
 
         elif my_option == "3":
             User.view()
+            User.delete_cred()
             
 
         else:
@@ -48,33 +52,39 @@ class Credentials:
 
     
 
-            
-            
-
-
-
-   
-
-
 class User:
     def __init__ (self, first_name, second_name, password):
         self.first_name = first_name
         self.second_name = second_name
         self.password = password
 
+    def signup():
+        user1 = User(input("Firstname: "), input("Secondname: "), input ("Password: "))
+        user.append(user1)
+        User.login()
+
     def login():
         print("KIndly repeat your details to login")
-        # firstname = input("Firstname: ")
-        # secondname = input("Secondname: ")
-        # password1 = input("Password: ")
-        Credentials.cred()
-        # for obj in user:
-        #     print(obj)
+        my_details = User(input("Firstname: "), input("Secondname: "), input ("Password: "))
+      
+        for obj in user:
+            if obj.first_name == my_details.first_name and obj.second_name == my_details.second_name and obj.password == my_details.password:
+                Credentials.cred()
+      
+            else:
+                User.login()
 
-        # if obj.firstname == first_name and obj.secondname == second_name and obj.password1 == password:
-        #     print("twendelee")
-        # else:
-        #     print("Matata")
+    def delete_cred():
+        print("Do you want to delete your credentials?")
+        print("1. Yes 2. No")
+        option = input()
+
+        if option == "1":
+            print("You have deleted your credentials")
+            details.clear()
+        else:
+            User.view()
+
 
     def view():
         print("Do you want to view your credentials?")
@@ -91,22 +101,12 @@ class User:
             Credentials.cred()
 
 
-    def signup():
-        user1 = User(input("Firstname: "), input("Secondname: "), input ("Password: "))
-        user.append("user1")
-        User.login()
-
     def password(length):
         letters = string.ascii_lowercase
         result = ''.join(random.choice(letters) for i in range(10))
         return result
 
     
-
-
-
-
-
 
 
 print("Hello, welcome! ")
